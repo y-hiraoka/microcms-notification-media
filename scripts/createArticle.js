f53e1d6188550f68aff24be9c5e6413407b058f6
y-hiraoka <62982380+y-hiraoka@users.noreply.github.com>
2023-03-12 16:27:01 +0900

    Locale を設定する

--- scripts/createArticle.js
+++ scripts/createArticle.js
@@ -1,5 +1,6 @@
 const { createClient } = require("microcms-js-sdk");
 const { format } = require("date-fns");
+const { ja } = require("date-fns/locale");
 const { faker } = require("@faker-js/faker");
 
 const main = async () => {
@@ -33,7 +34,7 @@ const main = async () => {
   client.create({
     endpoint: "articles",
     content: {
-      title: format(new Date(), "yyyy月M月d日 H時m分 の投稿"),
+      title: format(new Date(), "yyyy月M月d日 H時m分 の投稿", { locale: ja }),
       category: category.id,
       content: faker.lorem.paragraphs(10, "\n"),
       relatedArticles: [],
